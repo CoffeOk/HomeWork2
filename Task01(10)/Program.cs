65d5d5168ec622e9f0b1f442ed88602988e1d08a
Wall-e <you@mail.com>
2023-09-28 20:08:19 +0300

    Reshenie zadachi 10

---
+++ Task01(10)/Program.cs
@@ -0,0 +1,25 @@
+﻿// Ввод - трёхзначное число
+// Вывод - десяток этого числа 
+
+int n;
+Vvod:
+Console.Write ("введите трёхзначное число: ");
+while (!int.TryParse (Console.ReadLine(), out n))
+Console.Write ("введите трёхзначное число: ");
+
+int VivodDes (int x)
+{
+            x = (x / 10) % 10;
+            return x;
+}
+
+if (n >99 && n <1000)
+{
+int result = VivodDes(n);
+Console.WriteLine ($"{n} -> {result}");
+}
+else
+{
+    goto Vvod;
+} 
+
