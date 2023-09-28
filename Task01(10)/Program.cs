3db107b93d8580ead96e729a594fa88ea71e67bd
Wall-e <you@mail.com>
2023-09-28 21:24:34 +0300

    Reshenie zadachi 15

--- Task01(10)/Program.cs
+++ Task01(10)/Program.cs
@@ -3,23 +3,23 @@
 
 int n;
 Vvod:
-Console.Write ("введите трёхзначное число: ");
-while (!int.TryParse (Console.ReadLine(), out n))
-Console.Write ("введите трёхзначное число: ");
+Console.Write("введите трёхзначное число: ");
+while (!int.TryParse(Console.ReadLine(), out n))
+    Console.Write("введите трёхзначное число: ");
 
-int VivodDes (int x)
+int VivodDes(int x)
 {
-            x = (x / 10) % 10;
-            return x;
+    x = (x / 10) % 10;
+    return x;
 }
 
-if (n >99 && n <1000)
+if (n > 99 && n < 1000)
 {
-int result = VivodDes(n);
-Console.WriteLine ($"{n} -> {result}");
+    int result = VivodDes(n);
+    Console.WriteLine($"{n} -> {result}");
 }
 else
 {
     goto Vvod;
-} 
+}
 
---
+++ Task03(15)/Program.cs
@@ -0,0 +1,37 @@
+﻿int dayNum;
+Nedela:
+Console.Write("Введите целое число");
+while (!int.TryParse(Console.ReadLine(), out dayNum))
+    Console.Write("Введите целое число от 1 до 7");
+
+bool Vihodnie(int v)
+{
+    if (v > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Budni(int b)
+{
+    if (b < 6 && b > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+if (Vihodnie(dayNum))
+{
+    Console.Write($"{dayNum} -> да");
+}
+else if (Budni(dayNum))
+{
+    Console.Write($"{dayNum} -> нет");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+    goto Nedela;
+}
